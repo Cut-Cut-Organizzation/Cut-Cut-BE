dbb83ea5cfa5043395a1eeba1ae21063c5118c58
Flavio DELLA CORTE <flavio.dellacorte1997@gmail.com>
2024-10-15 11:52:33 +0200

    Project Initializzation

--- Cut_Cut.API/obj/Debug/net8.0/Cut_Cut.API.AssemblyInfo.cs
+++ Cut_Cut.API/obj/Debug/net8.0/Cut_Cut.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cut_Cut.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5299a32e5921d9ffaf90bd2c661f53d07793bc1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cut_Cut.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cut_Cut.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
