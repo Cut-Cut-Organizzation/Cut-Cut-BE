fb44f151d33ee4ad1eeb35602a968ef2346fdf79
Flavio DELLA CORTE <flavio.dellacorte1997@gmail.com>
2024-10-15 14:26:33 +0200

    Added DBcontext and first migration

--- Cut_Cut.API/Program.cs
+++ Cut_Cut.API/Program.cs
@@ -1,11 +1,32 @@
+using Cut_Cut.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//GET the current Enviroment
+var env = builder.Environment;
+
+
+// Configure the right appsettings.json for the actual enviroment
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+// GET connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DBConnection");
+
 // Add services to the container.
+builder.Services.AddDbContext<CutCutDbContext>(options =>
+    options.UseSqlite(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Working with SQLLite In Asp.net Core Web API", Version = "v1" });
+
+});
 
 var app = builder.Build();
 
